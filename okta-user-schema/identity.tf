665c03e55def81a99d450f506ef3afa2ad2a529f
lazertaco <82404950+lazertaco@users.noreply.github.com>
2022-05-18 12:23:12 -0500

    Update identity.tf

--- okta-user-schema/identity.tf
+++ okta-user-schema/identity.tf
@@ -11,17 +11,15 @@ terraform {
   }
 }
 
+provider "okta" {
+    org_name = var.org_name
+    base_url = var.base_url
+    api_token = var.api_token
+}
+
 resource "okta_user_schema_property" "dob_extension" {
   index  = "date_of_birth"
   title  = "Date of Birth"
   type   = "string"
   master = "PROFILE_MASTER"
-}
-
-
-
-provider "okta" {
-    org_name = var.org_name
-    base_url = var.base_url
-    api_token = var.api_token
 }
